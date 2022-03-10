aea559e21d231566370251f576be2210a9ca0418
Seyi <kuforiji98@gmail.com>
2022-03-10 19:33:30 +0100

    My low-level programming commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,6 @@
  */
 int main(void)
 {
-	char *S = "Programming is like building a multilingual puzzle\n;"
-
-	puts(S);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
