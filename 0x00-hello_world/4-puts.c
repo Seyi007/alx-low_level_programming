1f242afcdfc4b16bee7a71a8856044054893a632
Seyi <kuforiji98@gmail.com>
2022-03-10 19:00:44 +0100

    My low-level programming commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Entry points
+*outputs message unto screen
+*return: Always 0 (success)
+*/
+int main(void)
+{
+	char *S = "Programming is like building a multilingual puzzle";
+
+	puts(S);
+	return (0);
+}
