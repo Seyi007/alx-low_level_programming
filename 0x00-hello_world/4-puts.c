b53751f5b0ba65883a2ae39b948a79d550de6035
Seyi <kuforiji98@gmail.com>
2022-03-10 19:24:44 +0100

    My low-level programming commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	char *S = "Programming is like building a multilingual puzzle\n;
+	char *S = "Programming is like building a multilingual puzzle\n;"
 
 	puts(S);
 	return (0);
