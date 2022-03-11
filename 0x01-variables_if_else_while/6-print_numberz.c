9546aed03ad7c513cbf12e9345b9e03a85876af6
Seyi <kuforiji98@gmail.com>
2022-03-12 00:54:30 +0100

    My commits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+  #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
