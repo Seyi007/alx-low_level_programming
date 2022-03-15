f536c7f10e85e2407d40687f9653b1255c4a69f8
Seyi <kuforiji98@gmail.com>
2022-03-15 18:31:12 +0100

    My commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_islower - checks lowercase character
+ *@c: c is an ascii character
+ *
+ * Return: one if c is lowercase, zero if otherwise
+ */
+int _islower(int c)
+{
+	int c;
+	int a = 97;
+	int z = 122;
+
+	for (a < z)
+	if (c == 99)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
