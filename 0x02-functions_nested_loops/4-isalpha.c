7cce50fea253e40c4a8b227435ab11e84d5610cf
Seyi <kuforiji98@gmail.com>
2022-03-15 19:55:55 +0100

    My commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for lowercase
+ * @c: c is an ascii character
+ *
+ * Return: 1 if lowercase
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || C >= 'A' && C <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
