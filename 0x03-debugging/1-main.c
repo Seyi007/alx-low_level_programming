75379e006cea5c28ec2dd1543fdc690d13e00d55
Seyi <kuforiji98@gmail.com>
2022-03-16 16:44:44 +0100

    My commits

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,4 @@
-carrie@ubuntu:/debugging$ cat 1-main.c
-#include "main.h"
+#include <stdio.h>
 
 /**
 * main - causes an infinite loop
@@ -8,18 +7,15 @@ carrie@ubuntu:/debugging$ cat 1-main.c
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
+	i = 0;
+/*	while (i < 10) */
+/*	{ */
+/*	putchar(i); */
+/*	} */
+	printf("Infinite loop avoided! \\o/\n");
 
-        i = 0;
-
-/*	while (i < 10)*/
-        {
-                putchar(i);
-        }
-
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+	return (0);
 }
