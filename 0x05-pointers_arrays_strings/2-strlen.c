b096e34f28f3f872fadfaac1f2a80b5bc618e7a5
Seyi <kuforiji98@gmail.com>
2022-03-21 18:31:07 +0100

    My commits

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_strlen - prints the lenght of a string
+ *@s: variable name for string
+ *
+ *Return: 0 always
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+	return (len);
+
+}
