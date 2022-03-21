d15c589dbe5973a263e8f59ddad97aa74cecc72d
Seyi <kuforiji98@gmail.com>
2022-03-21 18:54:09 +0100

    My commits

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_puts - prints a string in stdio
+ *@str: string variable
+ *
+ *Return: 0 always
+ */
+
+void _puts(char *str)
+{
+	*str[] = "Hello";
+	return (*str[]);
+
+}
