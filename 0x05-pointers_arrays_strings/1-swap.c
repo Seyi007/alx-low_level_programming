80fd0f70e22dece436790458b2da08cf4d0f04e0
Seyi <kuforiji98@gmail.com>
2022-03-21 17:49:46 +0100

    My commits

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *swap_int - swaps the value of two integers
+ *@a: variable a
+ *@b: variable b
+ *Return: 0 always
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = &b;
+	*b = &a;
+}
