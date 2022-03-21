454870bed3f57ba37cbce600c8fc230d064a1ae6
Seyi <kuforiji98@gmail.com>
2022-03-21 19:05:16 +0100

    My commits

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_rev - prints a reverse string in stdio
+ *@s: string variable
+ *
+ *Return: 0 always
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (str[i] = '\0'; i > 0; i--)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
