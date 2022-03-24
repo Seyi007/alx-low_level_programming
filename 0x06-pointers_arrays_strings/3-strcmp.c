1ec61971aade19f28798aa8753b7b678a241d794
Seyi <kuforiji98@gmail.com>
2022-03-24 09:27:21 +0100

    My commits

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_strcmp - compares two string
+ *@s1: variable
+ *@s2: variable
+ *
+ *Return: 0 always
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (s1 != '\0'; s2 != '\0' && s1 == s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (s1 == s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (s1 - s2);
+	}
+
+}
