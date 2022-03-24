5c3b8a3ecbde9fceac4fa9fdd561df3afbd9dde9
Seyi <kuforiji98@gmail.com>
2022-03-24 10:30:38 +0100

    My commits

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *reverse_array - reverse the content of an array
+ *@a: an array of integers
+ *@n: number of elements to swap
+ *
+ *Return: 0
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, x;
+
+	j = n - 1;
+
+	for (i = 0; i < n; i++)
+	{
+		x = a[i]
+		a[i] = a[j]
+		a[j--] = x
+	}
+}
