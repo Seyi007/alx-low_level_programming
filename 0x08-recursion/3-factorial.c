31bf17cbfcb95d5e21b3a98d4e49ed9ca2b6f9d0
Seyi <kuforiji98@gmail.com>
2022-03-30 17:47:42 +0100

    My commits

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *factorial - prints the factorial of a given number
+ *@n: integer variable
+ *
+ *Return: 0 always
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n -1));
+}
+
