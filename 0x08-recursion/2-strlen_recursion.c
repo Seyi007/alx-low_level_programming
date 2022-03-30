49186c4dfbb7ab768392ff3d66f9503535adfd5e
Seyi <kuforiji98@gmail.com>
2022-03-30 17:29:38 +0100

    My commits

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_strlen_recursion - prints the length of string
+ *using recursion
+ *@s: pointer variable
+ *
+ *Return: 0 always
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s < '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
+
