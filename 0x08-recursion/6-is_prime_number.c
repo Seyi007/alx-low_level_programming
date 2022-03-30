f8767132c98dd2c54cdf7d4d2d4628e0ac34b068
Seyi <kuforiji98@gmail.com>
2022-03-30 20:42:22 +0100

    My commits

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+int actual_prime(int n, int i);
+
+
+/**
+ *is_prime_number - prints 1 if number is even
+ *prints 0 if otherwise
+ *@n: number to be processed
+ *
+ *Return: 0 always
+*/
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * _prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int _prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (_prime(n, i - 1));
+}
