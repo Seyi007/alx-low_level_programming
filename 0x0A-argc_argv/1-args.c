f78c33033c93f12e78d4a03efc03a94d39012e7b
Seyi <kuforiji98@gmail.com>
2022-04-02 22:04:30 +0100

    My commits

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - This program prints the number of arguments.
+ *@argc: the argument count
+ *@argv: the pointer to the array of strings
+ *Return: 0 always
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
