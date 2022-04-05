657a48c8d7c685affe1955917cc6a025978e837a
Seyi <kuforiji98@gmail.com>
2022-04-05 15:06:20 +0100

    My commits

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		ptr[i] = (int *) malloc(sizeof(int) * width);
-		if (ptr == NULL)
+		if (ptr[i] == NULL)
 		{
 			free(ptr);
 			for (j = 0; j <= i; j++)
