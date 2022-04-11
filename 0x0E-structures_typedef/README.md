431de80f690d6cc95e63b809b9eac92e6a5562d6
Seyi <kuforiji98@gmail.com>
2022-04-11 16:58:43 +0100

    My commits

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains progects on structs and typedefs.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure of dog characteristics
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: a struct of dog's name, age and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
