344aefcfe6bb58eaf873e3635119b5abdf24f6a5
Seyi <kuforiji98@gmail.com>
2022-04-21 21:38:48 +0100

    My commits

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *add_node - adds a new node at the start of the lonked list
+ *@head: pointer to pointer variable
+ *@str: string to be duplicated
+ *
+ *Return: the address of the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int count = 0;
+
+	while (str[count])
+		count++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = count;
+	new->next = (*head);
+	(*head) = new;
+
+	return (*head);
+}
