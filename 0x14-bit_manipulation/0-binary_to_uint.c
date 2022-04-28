e9a49d2920091dba7445f1b2ef6beb63372beb16
Seyi <kuforiji98@gmail.com>
2022-04-28 15:37:38 +0100

    My commits

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,14 +19,14 @@ unsigned int binary_to_uint(const char *b)
 
 	len = strlen(b);
 
-	for (c = len-1; c >= 0; c--)
+	for (c = len - 1; c >= 0; c--)
 	{
 		if (b[c] != '0' && b[c] != '1')
 			return (0);
 
-		if(b[c] == '1')
+		if (b[c] == '1')
 		{
-			i +=k;
+			i += k;
 		}
 		k *= 2;
 	}
